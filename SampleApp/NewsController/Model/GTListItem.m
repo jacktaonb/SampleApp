5cf676e96b5573e4e9428b45c310ad3393f779ad
jacktaonb <wangtaotrying@qq.com>
2020-12-07 14:24:06 +0800

    序列化和反序列化

--- SampleApp/NewsController/Model/GTListItem.m
+++ SampleApp/NewsController/Model/GTListItem.m
@@ -9,6 +9,39 @@
 
 @implementation GTListItem
 
+#pragma mark - NSSecureCoding
+
+- (void)encodeWithCoder:(nonnull NSCoder *)acoder {
+    [acoder encodeObject:self.category forKey:@"category"];
+    [acoder encodeObject:self.picUrl forKey:@"picUrl"];
+    [acoder encodeObject:self.uniqueKey forKey:@"uniqueKey"];
+    [acoder encodeObject:self.title forKey:@"title"];
+    [acoder encodeObject:self.date forKey:@"date"];
+    [acoder encodeObject:self.authorName forKey:@"authorName"];
+    [acoder encodeObject:self.articleUrl forKey:@"articleUrl"];
+    
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)aDecoder {
+    self = [super self];
+    if (self) {
+        self.category = [aDecoder decodeObjectForKey:@"category"];
+        self.picUrl = [aDecoder decodeObjectForKey:@"picUrl"];
+        self.uniqueKey = [aDecoder decodeObjectForKey:@"uniqueKey"];
+        self.title = [aDecoder decodeObjectForKey:@"title"];
+        self.date = [aDecoder decodeObjectForKey:@"date"];
+        self.authorName = [aDecoder decodeObjectForKey:@"authorName"];
+        self.articleUrl = [aDecoder decodeObjectForKey:@"articleUrl"];
+    }
+    return self;
+}
+
++ (BOOL)supportsSecureCoding{
+    return YES;
+}
+
+#pragma mark - public method
+
 - (void)configWithDictionary:(NSDictionary *)dictionary {
     self.category = [dictionary objectForKey:@"category"];
     self.picUrl = [dictionary objectForKey:@"thumbnail_pic_s"];
@@ -18,5 +51,7 @@
     self.authorName = [dictionary objectForKey:@"author_name"];
     self.articleUrl = [dictionary objectForKey:@"url"];
 }
+
+
 
 @end
--- SampleApp/NewsController/Model/GTListLoader.m
+++ SampleApp/NewsController/Model/GTListLoader.m
@@ -14,7 +14,10 @@
     NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=1113811726e766953e642681e1371677";
     NSURL *listURL = [NSURL URLWithString:urlString];
     NSURLSession *session = [NSURLSession sharedSession];
+    
+    __weak typeof(self) weakself = self;
     NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData *_Nullable data, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+        __weak typeof(weakself) strongSelf = weakself;
         NSError *jsonError;
         id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
         NSArray *dataArray = [((NSDictionary *)[((NSDictionary *)jsonObj) objectForKey:@"result"]) objectForKey:@"data"];
@@ -24,7 +27,7 @@
             [listItem configWithDictionary:info];
             [listItemArray addObject:listItem];
         }
-
+        [weakself _archiveListDataWithArray:listItemArray.copy];
         dispatch_async(dispatch_get_main_queue(), ^{
                            if (finishBlock) {
                                finishBlock(error == nil, listItemArray.copy);
@@ -33,6 +36,50 @@
     }];
 
     [dataTask resume];
+}
+
+- (void)_archiveListDataWithArray:(NSArray<GTlistItem *> *)array {
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *cachePath = [pathArray firstObject];
+
+    NSFileManager *fileManger = [NSFileManager defaultManager];
+    
+    //创建文件夹
+    NSString *dataPath = [cachePath stringByAppendingPathComponent:@"GTData"];
+    NSError *creatError;
+    [fileManger createDirectoryAtPath:dataPath withIntermediateDirectories:YES attributes:nil error:&creatError];
+    
+    //创建文件
+    NSString *listDataPath = [dataPath stringByAppendingPathComponent:@"list"];
+    NSData *listData = [NSKeyedArchiver archivedDataWithRootObject:array requiringSecureCoding:YES error:nil];
+    [fileManger createFileAtPath:listDataPath contents:listData attributes:nil];
+    
+    NSData *readListData = [fileManger contentsAtPath:listDataPath];
+    
+  //  id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class],[GTListItem class], nil] fromData:readListData error:nil];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:listData forKey:@"listData"];
+    
+    NSData *testListdata = [[NSUserDefaults standardUserDefaults] dataForKey:@"listData"];
+    
+    id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class],[GTListItem class], nil] fromData:testListdata error:nil];
+//    //查询文件
+//    BOOL fileExist = [fileManger fileExistsAtPath:listDataPath];
+    
+    //删除
+//    if (fileExist) {
+//        [fileManger removeItemAtPath:listDataPath error:nil];
+//    }
+    
+    NSLog(@"");
+//
+//    NSFileHandle *fileHandle = [NSFileHandle fileHandleForUpdatingAtPath:listDataPath];
+//
+//    [fileHandle seekToEndOfFile];
+//    [fileHandle writeData:[@"def" dataUsingEncoding:NSUTF8StringEncoding]];
+//
+//    [fileHandle synchronizeFile];
+//    [fileHandle closeFile];
 }
 
 @end
